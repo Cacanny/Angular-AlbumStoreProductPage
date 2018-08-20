3cc778a7f311a4d54665390d3fb7847d6bae508c
Danny Hoeve <d.hoeve@arcady.nl>
2018-08-20 16:24:08 +0200

    completed the module

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,7 +1,6 @@
 import { Track } from './track';
 
 export interface Album {
-
     name: string;
     releaseDate: string;
     coverImage: string;
--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+
   }
 
 }
